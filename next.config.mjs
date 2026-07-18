fb36640614364b8e9ed55cb357dd347e76305033
codeinfinite19 <shahedhassan572@gmail.com>
2026-07-19 02:12:43 +0600

    login with google is error

--- next.config.mjs
+++ next.config.mjs
@@ -7,6 +7,15 @@ const nextConfig = {
       { protocol: "https", hostname: "res.cloudinary.com" },
     ],
   },
+
+  async rewrites() {
+    return [
+      {
+        source: "/api/auth/:path*",
+        destination: `${process.env.NEXT_PUBLIC_API_URL}/api/auth/:path*`,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
--- src/lib/auth-client.js
+++ src/lib/auth-client.js
@@ -1,5 +1,8 @@
 import { createAuthClient } from "better-auth/react";
 
 export const authClient = createAuthClient({
-  baseURL: process.env.NEXT_PUBLIC_API_URL,
+  baseURL:
+    typeof window !== "undefined"
+      ? `${window.location.origin}/api/auth`
+      : `${process.env.NEXT_PUBLIC_APP_URL}/api/auth`,
 });
